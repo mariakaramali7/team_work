c02c36053c1841945ae7c32c939d64b458e5f038
liakos20 <i.harbilas20@gmail.com>
2018-02-13 18:18:03 +0200

    oloklhrh h main

--- Test.java
+++ Test.java
@@ -108,3 +108,122 @@ public class Test {
 		input.close();
 	}
 
+public static void print()   //emfanish basikou menou sto xrhsth
+	{
+		System.out.println("********** Welcome to our Library **********");
+		System.out.println("Menu:");
+		System.out.println("Type 1 to display books' list.");
+		System.out.println("Type 2 to display authors' list.");
+		System.out.println("Type 3 to search for books.");
+		System.out.println("Type 4 to create new Author.");
+		System.out.println("Type 5 to create new Book.");
+		System.out.println("Type 6 to display newest book.");
+		System.out.println("Type 7 change book's publication year.");
+		System.out.print("Type \"exit\" to terminate the program.\n\tType here: ");
+	}
+
+	public static void displayBooks()
+	{
+		System.out.println("\n\n***** Books' List *****\n");
+
+		for(int i = 0; i < Book.books.length && Book.books[i] != null; i++)
+		{
+			System.out.println(Book.books[i].toString());
+		}
+
+		System.out.println();
+	}
+
+	public static void displayAuthors()
+	{
+		System.out.println("\n\n***** Authors' List *****\n");
+
+		for(int i = 0; i < Author.authors.length && Author.authors[i] != null; i++)
+		{
+			System.out.println(Author.authors[i].toString());
+		}
+
+		System.out.println();
+	}
+
+	public static void searchBooks(String name)
+	{
+		boolean found = false;
+
+		for(int i = 0; i < Book.books.length && Book.books[i] != null; i++ )
+		{
+			String author = Book.books[i].getAuthor().getName();
+			if( author.equals(name))
+			{
+				System.out.println("\n\n***** Display " + name + "'s Books *****");
+				System.out.println(Book.books[i].toString());
+				found = true;
+			}
+		}
+
+		System.out.println();
+
+		if(!found)
+		{
+			System.out.println("\n\n***** Error! No author with name: [" + name + "] found. *****\n");
+		}
+	}
+
+	public static void createAuthor(String name, String nationality, int numberOfWorks)
+	{
+		if(Author.authors[9] != null)
+		{
+			System.out.println("\n\n***** Error! Memory is full. *****\n");
+		}
+		else
+		{
+			Author newAuthor = new Author(name, nationality, numberOfWorks);
+			System.out.println("\n\n***** Successfully created " + newAuthor.toString() + " *****\n");
+		}
+	}
+
+	public static void createBook(String title, int id, String genre, int yearOfPublication)
+	{
+		if(Book.books[9] != null)
+		{
+			System.out.println("\n\n***** Error! Memory is full. *****\n");
+		}
+		else
+		{
+			Author auth = Author.authors[id-1];
+			Book newBook = new Book(title, auth, genre, yearOfPublication);
+			System.out.println("\n\n***** Successfully created " + newBook.toString() + " *****\n");
+		}
+	}
+
+	public static void showNewestBook()
+	{
+		System.out.println("\n\n***** Newest Book *****\n");
+
+		Book newestBook = Book.books[0];
+
+		for(int i = 0; i < Book.books.length && Book.books[i] != null; i++ )
+		{
+			if(Book.books[i].getYearOfPublication() >= newestBook.getYearOfPublication() )
+			{
+				newestBook = Book.books[i];
+			}
+		}
+		System.out.println(newestBook.toString());
+		System.out.println();
+	}
+
+	public static void changePublicationYear(int id, int yearOfPublication)
+	{
+		if(yearOfPublication > 0)
+		{
+			Book.books[id-1].setYearOfPublication(yearOfPublication);
+			System.out.println("\n\n***** Successfully changed the publication year! *****\n");
+		}
+		else
+		{
+			System.out.println("\n\n***** Error! Non positive integer was inserted. *****\n");
+		}
+
+	}
+}
